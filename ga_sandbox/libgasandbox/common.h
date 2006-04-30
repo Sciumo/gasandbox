5664b9a67b7e69ba53d7188b46d8baad7e436d8f
fontijne <fontijne@ce2f24c1-15a1-e74c-aed6-c10be1f8d102>
2006-04-30 19:26:59 +0000

    Added ex3g2 (temp)

--- ga_sandbox/libgasandbox/common.h
+++ ga_sandbox/libgasandbox/common.h
@@ -19,8 +19,11 @@
 #include <math.h>
 
 #ifndef M_PI
-#define M_PI 3.1415927
+#define M_PI 3.14159265358979323846
 #endif 
+
+#define U_RAD2DEG( x ) ( (x) * (float)180.0 / (float)M_PI)
+#define U_DEG2RAD( x ) ( (x) * (float)M_PI / (float)180.0)
 
 
 #endif /* _COMMON_H_ */
