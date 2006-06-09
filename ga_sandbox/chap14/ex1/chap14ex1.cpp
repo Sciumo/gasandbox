90616bb3e78b656277485b1417f7e22b831087d6
fontijne <fontijne@ce2f24c1-15a1-e74c-aed6-c10be1f8d102>
2006-06-09 21:08:57 +0000

    dff

--- ga_sandbox/chap14/ex1/chap14ex1.cpp
+++ ga_sandbox/chap14/ex1/chap14ex1.cpp
@@ -26,6 +26,7 @@
 #include <vector>
 #include <set>
 #include <string>
+#include <utility>
 
 #include <libgasandbox/common.h>
 #include <libgasandbox/c2ga.h>
@@ -52,6 +53,16 @@ const char *WINDOW_TITLE = "Geometric Algebra, Chapter 14, Example 1: Vonoroi Di
 int g_viewportWidth = 800;
 int g_viewportHeight = 600;
 int g_GLUTmenu;
+
+#define DRAW_VONOROI 1
+#define DRAW_DELAUNAY 2
+
+int g_drawMode = DRAW_DELAUNAY;
+
+#define DRAG_POINTS 1
+#define CREATE_POINTS 2
+
+int g_mouseMode = DRAG_POINTS;
 
 // what point to drag (or -1 for none)
 int g_dragPoint = -1; 
@@ -90,7 +101,7 @@ public:
 			m_neighborTriangles[2] = -1;
 	}
 
-	DelaunayTriangle(int vtxIdx[3]) {
+	DelaunayTriangle(int vtxIdx[3], const circle &C) : m_c(C) {
 		m_vertices[0] = vtxIdx[0];
 		m_vertices[1] = vtxIdx[1];
 		m_vertices[2] = vtxIdx[2];
@@ -99,7 +110,7 @@ public:
 			m_neighborTriangles[2] = -1;
 	}
 	
-	DelaunayTriangle(const DelaunayTriangle &T) {
+	DelaunayTriangle(const DelaunayTriangle &T) : m_c(T.m_c) {
 		m_vertices[0] = T.m_vertices[0];
 		m_vertices[1] = T.m_vertices[1];
 		m_vertices[2] = T.m_vertices[2];
@@ -116,6 +127,7 @@ public:
 			m_neighborTriangles[0] = T.m_neighborTriangles[0];
 			m_neighborTriangles[1] = T.m_neighborTriangles[1];
 			m_neighborTriangles[2] = T.m_neighborTriangles[2];
+			m_c = T.m_c;
 		}
 	}
 	
@@ -127,6 +139,8 @@ public:
 	*/
 	int m_neighborTriangles[3];
 
+	/// circle through the three vertices
+	circle m_c;
 };
 
 class DelaunayTriangulation {
@@ -153,8 +167,8 @@ DelaunayTriangulation computeDelaunayTriangulation(const std::vector<normalizedP
 	// init qhull
 	{
 		qh_init_A (stdin, stdout, stderr, 0, NULL);
-			if (setjmp (qh errexit)) {
-				fprintf(stderr, "qhull generated error\n");
+		if (setjmp (qh errexit)) {
+			fprintf(stderr, "qhull generated error\n");
 		}
 		qh_initflags("qhull Qx i s Tcv C-0");
 	}
@@ -207,21 +221,29 @@ DelaunayTriangulation computeDelaunayTriangulation(const std::vector<normalizedP
 					// create vertex:				
 					DT.m_vertices[idx] = DelaunayVertex(
 						normalizedPoint(normalizedPoint_e1_e2_ni_nof1_0, e1C, e2C, niC));
-						
-					printf("Creating vertex %d (%f %f %f)\n", idx, e1C, e2C, niC);
 				}
-				
-				// add triangle idx to vertex:
-				DT.m_vertices[idx].m_triangles.insert((int)DT.m_triangles.size());
-				printf("Adding triangle %d to vertex %d\n", (int)DT.m_triangles.size(), idx);
 				
 				// add vertex index to triangle:
 				vtxIdx[i++] = idx;
 			}
+
 			
+			DelaunayVertex &V1 = DT.m_vertices[vtxIdx[0]];
+			DelaunayVertex &V2 = DT.m_vertices[vtxIdx[1]];
+			DelaunayVertex &V3 = DT.m_vertices[vtxIdx[2]];
+		
+			// check if `front-facing':
+			circle C = _circle(V1.m_pt ^ V2.m_pt ^ V3.m_pt);
+			if (_Float((C) << (e1 ^ e2 ^ ni)) < 0.0) continue;
+			
+			// add triangle idx to vertices:
+			int triangleIdx = (int)DT.m_triangles.size();
+			V1.m_triangles.insert(triangleIdx);
+			V2.m_triangles.insert(triangleIdx);
+			V3.m_triangles.insert(triangleIdx);
+				
 			// create new triangle:
-			DT.m_triangles.push_back(DelaunayTriangle(vtxIdx));
-			printf("Creating triangle %d %d %d\n", vtxIdx[0], vtxIdx[1], vtxIdx[2]);
+			DT.m_triangles.push_back(DelaunayTriangle(vtxIdx, C));
 
 			qh_settempfree(&vertices);			
 		}
@@ -231,28 +253,28 @@ DelaunayTriangulation computeDelaunayTriangulation(const std::vector<normalizedP
 		qh_memfreeshort(&qhCurlong, &qhTotlong);
 		qhCoord.clear();
 	}
-	printf("All done!\n"); fflush(stdout);
 	
 	// construct the 'neigboring triangle info':
 	// for all faces, check all pairs vertices [0, 1], [1, 2], [2, 0]
 	// when two vertices are used in the same triangle (not equal to this
 	// triangle), then that is the neighboring triangle
-/*	for (unsigned int i = 0; i < DT.m_triangles.size(); i++) {
+	for (unsigned int i = 0; i < DT.m_triangles.size(); i++) {
 		DelaunayTriangle &T = DT.m_triangles[i];
+
 		for (int j = 0; j < 3; j++) {
 			const DelaunayVertex &V1 = DT.m_vertices[T.m_vertices[j]];
 			const DelaunayVertex &V2 = DT.m_vertices[T.m_vertices[(j+1)%3]];
-			// todo: intersect sets of used triangles!!!
+						
+			// todo: use set union of STL
 			for (std::set<int>::const_iterator I = V1.m_triangles.begin(); 
 				I != V1.m_triangles.end(); I++) {
 				if (*I == i) continue; // must not be this triangle!
-				if (V1.m_triangles.find(*I) != V1.m_triangles.end()) {
+				if (V2.m_triangles.find(*I) != V2.m_triangles.end()) {
 					T.m_neighborTriangles[j] = *I;
-					printf("triangle %d neighbors triangle %d\n", *I, i);
 				}
 			}
 		}
-	}*/
+	}
 	
 	return DT;
 }
@@ -261,12 +283,8 @@ DelaunayTriangulation computeDelaunayTriangulation(const std::vector<normalizedP
 
 
 void display() {
-	printf("Again?\n");fflush(stdout); 
-	{
-		// compute the triangulation:
-		DelaunayTriangulation DT = computeDelaunayTriangulation(g_points);
-	}
-	printf("Hoi!\n"); fflush(stdout);
+	// compute the triangulation:
+	DelaunayTriangulation DT = computeDelaunayTriangulation(g_points);
 
 	glViewport(0, 0, g_viewportWidth, g_viewportHeight);
 	glMatrixMode(GL_PROJECTION);
@@ -288,8 +306,6 @@ void display() {
 
 //	glTranslatef((float)g_viewportWidth / 2.0f, (float)g_viewportHeight / 2.0f, 0.0f);
 
-	printf("Hoi!\n"); fflush(stdout);
-	
 	g_drawState.m_pointSize	= 100.0f;
 	glColor3f(1.0f, 0.0f, 0.0f);
 	// draw the points:
@@ -297,22 +313,100 @@ void display() {
 		if (GLpick::g_pickActive) glLoadName(i);
 		draw(g_points[i]);
 	}
+	
+	if ((!GLpick::g_pickActive) && (g_drawMode == DRAW_DELAUNAY)) {
+		// draw the Delaunay triangulation
+		glColor3f(0.0f, 0.0f, 1.0f);
+		for (unsigned int i = 0; i < DT.m_triangles.size(); i++) {
+			const DelaunayTriangle &T = DT.m_triangles[i];
+			const DelaunayVertex &V1 = DT.m_vertices[T.m_vertices[0]];
+			const DelaunayVertex &V2 = DT.m_vertices[T.m_vertices[1]];
+			const DelaunayVertex &V3 = DT.m_vertices[T.m_vertices[2]];
+			
+			glBegin(GL_LINE_LOOP);
+				glVertex2f(V1.m_pt.e1(), V1.m_pt.e2());
+				glVertex2f(V2.m_pt.e1(), V2.m_pt.e2());
+				glVertex2f(V3.m_pt.e1(), V3.m_pt.e2());			
+			glEnd();
+		}
+	}
+	
+	if ((!GLpick::g_pickActive) && (g_drawMode == DRAW_VONOROI)) {
+		// draw the Vonoroi diagram
+		glLineStipple(1, 0x0F0F);
+		
+		// set of pair<triangleIdx, triangleIdx> that lists edges that were already drawn
+		// (to avoid double work)
+//		std::set<std::pair<int, int> > drawnEdges;
+		
+		glColor3f(0.0f, 1.0f, 0.0f);
+		for (unsigned int i = 0; i < DT.m_triangles.size(); i++) {
+			// visit each triangle:
+			const DelaunayTriangle &T1 = DT.m_triangles[i];
+						
+			// get center point of T1:
+			normalizedPoint C1 = _normalizedPoint(-T1.m_c * inverse(ni << T1.m_c));
+			
+			for (int j = 0; j < 3; j++) {
+				// visit each edge:
+				if (T1.m_neighborTriangles[j] < 0) {
+					// from here to infinity!
+					const DelaunayVertex &V1 = DT.m_vertices[T1.m_vertices[j]];
+					const DelaunayVertex &V2 = DT.m_vertices[T1.m_vertices[(j+1)%3]];
+					
+					// get edge-mid-point direction(hack):
+					vectorE2GA D = _vectorE2GA(unit_e(_vectorE2GA(0.5f * (V1.m_pt + V2.m_pt) - C1)));
+					
+					// possibly flip edge-mid-point direction (on which side of the edge is the center point?)
+					if (_Float(dual(V1.m_pt ^ V2.m_pt ^ ni) << C1) < 0.0f)
+						D = -D;
+					
+					glEnable(GL_LINE_STIPPLE);
+					glBegin(GL_LINES);
+					glVertex2f(C1.e1(), C1.e2());
+					glVertex2f(C1.e1() + 100.0f * D.e1(), C1.e2() + 100.0f * D.e2());
+					glEnd();
+					glDisable(GL_LINE_STIPPLE);
+				
+				}
+				else if (T1.m_neighborTriangles[j] <= i)
+					continue; // do not draw edges twice!
+				else {
+					// from here to center of other triangle
+					const DelaunayTriangle &T2 = DT.m_triangles[T1.m_neighborTriangles[j]];
+					
+					// get center point of T2:
+					normalizedPoint C2 = _normalizedPoint(-T2.m_c * inverse(ni << T2.m_c));
+					
+					// draw bisector:
+					glBegin(GL_LINES);
+					glVertex2f(C1.e1(), C1.e2());
+					glVertex2f(C2.e1(), C2.e2());
+					glEnd();
+				
+				}
+				
+			}
+		}
+	}
+	
+	
 		
 	glPopMatrix();
 	
 	if (!GLpick::g_pickActive) {
 		glColor3f(0.0, 0.0, 0.0);
 		void *font = GLUT_BITMAP_HELVETICA_12;
-		renderBitmapString(20, 20, font, "blah");
+		renderBitmapString(20, g_viewportHeight - 20, font, (g_mouseMode == DRAG_POINTS) ? "Mouse mode: DRAG points" : "Mouse mode: CREATE points");
+		renderBitmapString(g_viewportWidth / 2, g_viewportHeight - 20, font, (g_drawMode == DRAW_VONOROI) ? "Draw mode: VONOROI" : "Draw mode: DELAUNAY");
+		
+		renderBitmapString(20, 40, font, "Use the left mouse button to create and drag points (depending on mouse mode).");
+		renderBitmapString(20, 20, font, "Use the other mouse buttons to access the popup menu (to select mouse mode, select draw mode).");
 	}
-	
-	printf("Hoi!\n"); fflush(stdout);
 	
 	if (!GLpick::g_pickActive) {
 		glutSwapBuffers();	
 	}
-	
-	printf("Hoi!!!!!!!\n"); fflush(stdout);
 }
 
 void reshape(GLint width, GLint height) {
@@ -328,8 +422,24 @@ c2ga::vectorE2GA mousePosToVector(int x, int y) {
 }
 
 void MouseButton(int button, int state, int x, int y) {
-	g_dragPoint = pick(x, g_viewportHeight - y, display, NULL); // NULL = pointer to distance
+	if (state == GLUT_UP) return;
+	
+	if (g_mouseMode == DRAG_POINTS) {
+		g_dragPoint = pick(x, g_viewportHeight - y, display, NULL); // NULL = pointer to distance
+	}
+	else if (g_mouseMode == CREATE_POINTS) {
+		g_dragPoint = pick(x, g_viewportHeight - y, display, NULL); // NULL = pointer to distance
+		if (g_dragPoint >= 0) {
+			// when you click too close to an existing point, switch to drag mode:
+			g_mouseMode = DRAG_POINTS;
+		}
+		else g_points.push_back(c2gaPoint(x, g_viewportHeight - y));
+	}
+	
 	g_prevMousePos = mousePosToVector(x, y);
+	
+	// redraw viewport
+	glutPostRedisplay();
 }
 
 void MouseMotion(int x, int y) {
@@ -337,10 +447,12 @@ void MouseMotion(int x, int y) {
 	c2ga::vectorE2GA mousePos = mousePosToVector(x, y);
 	c2ga::vectorE2GA motion = _vectorE2GA(mousePos - g_prevMousePos);
 	
-	if (g_dragPoint >= 0) {
-		// translate point
-		normalizedTranslator T = _normalizedTranslator(1.0f - (0.5f * motion ^ ni));
-		g_points[g_dragPoint] = _normalizedPoint(T * g_points[g_dragPoint] * inverse(T));
+	if (g_mouseMode == DRAG_POINTS) {
+		if (g_dragPoint >= 0) {
+			// translate point
+			normalizedTranslator T = _normalizedTranslator(1.0f - (0.5f * motion ^ ni));
+			g_points[g_dragPoint] = _normalizedPoint(T * g_points[g_dragPoint] * inverse(T));
+		}
 	}
 
 	// remember mouse pos for next motion:
@@ -355,7 +467,12 @@ void Keyboard(unsigned char key, int x, int y) {
 }
 
 void menuCallback(int value) {
+	if (value < 0) {
+		g_mouseMode = -value;
+	}
+	else g_drawMode = value;
 
+	
 	// redraw viewport
 	glutPostRedisplay();	
 }
@@ -379,10 +496,10 @@ int main(int argc, char*argv[]) {
 	glutMotionFunc(MouseMotion);
 	
 	g_GLUTmenu = glutCreateMenu(menuCallback);
-	glutAddMenuEntry("Vonoroi Diagram", 0);
-	glutAddMenuEntry("Delaunay Triangulation", 1);
-	glutAddMenuEntry("Mode: Drag Points", -1);
-	glutAddMenuEntry("Mode: Create Points", -2);
+	glutAddMenuEntry("Vonoroi Diagram", DRAW_VONOROI);
+	glutAddMenuEntry("Delaunay Triangulation", DRAW_DELAUNAY);
+	glutAddMenuEntry("Mode: Drag Points", -DRAG_POINTS);
+	glutAddMenuEntry("Mode: Create Points", -CREATE_POINTS);
 	glutAttachMenu(GLUT_MIDDLE_BUTTON);
 	glutAttachMenu(GLUT_RIGHT_BUTTON);
 
@@ -391,6 +508,9 @@ int main(int argc, char*argv[]) {
 	g_points.push_back(c2gaPoint(300, 400));
 	g_points.push_back(c2gaPoint(100, 400));
 	g_points.push_back(c2gaPoint(250, 450));
+	g_points.push_back(c2gaPoint(700, 500));
+	g_points.push_back(c2gaPoint(400, 300));
+	g_points.push_back(c2gaPoint(650, 150));
 	
 
 
